f7811f7c620f4e7552835bc791a0077a0c2157cf
dennis <dennis.zhang@cargosmart.com>
2017-12-04 14:22:33 +0800

    fix code

--- extract_all.py
+++ extract_all.py
@@ -18,7 +18,7 @@ class ExtractAll():
         self.cfg.read("config.ini")        
         cmdb_host = self.cfg.get("cmdb","host")
         cmdb_port = self.cfg.get("cmdb","port")
-        cmdb_user = self.cfg.get("cmdb","user")ps_map_dict
+        cmdb_user = self.cfg.get("cmdb","user")
         cmdb_passwd = self.cfg.get("cmdb","passwd")
         cmdb_db = self.cfg.get("cmdb","db")
         cmdb_str = "mongodb://%s:%s@%s:%s" % (cmdb_user,cmdb_passwd,cmdb_host,cmdb_port)
