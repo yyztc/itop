ec705762ff715bdb903da04a859f812eb7b11c43
dennis <dennis.zhang@cargosmart.com>
2017-12-27 16:50:12 +0800

    trans appsolution fix

--- trans_appsolution.py
+++ trans_appsolution.py
@@ -6,6 +6,7 @@ import json
 import logging
 from logging.config import fileConfig
 import configparser
+import pdb
 
 fileConfig('logger_config.ini')
 logger=logging.getLogger('infoLogger')
@@ -43,7 +44,18 @@ class TransAppSolution():
         merge_appsolution_df['merge_environment'] = appsolution_df['zabbix_environment']
         merge_appsolution_df['merge_hosts'] = appsolution_df['zabbix_hosts'].map(lambda x:self.get_hostname_list(x))
         merge_appsolution_df = merge_appsolution_df[merge_appsolution_df['merge_name'].notnull()]
+
+        appsolution_coll2 = self.db['merge_virtualmachine']
+        appsolution_df2 = pd.DataFrame(list(appsolution_coll2.find()))
+        temp = appsolution_df2.loc[appsolution_df2.merge_app != '',['merge_name','merge_app','merge_env']]
+        temp['group_key'] = temp['merge_app'].values + '|'+ temp['merge_env'].values
+        temp = temp.groupby('group_key').aggregate(lambda x: list(x))[['merge_name']].rename(columns={'merge_name':'merge_hosts'})
+        temp=temp.reset_index()
+        temp['merge_name'] = temp['group_key'].map(lambda x:x.split('|')[0].strip()) 
+        temp['merge_environment'] = temp['group_key'].map(lambda x:x.split('|')[1].strip()) 
+        merge_appsolution_df2=temp[['merge_name','merge_environment','merge_hosts']]
         # logger.info(merge_appsolution_df.head(20))
+        merge_appsolution_df=merge_appsolution_df.append(merge_appsolution_df2)
         self.write_to_cmdb(coll_name='merge_appsolution',df=merge_appsolution_df)
         self.client.close()
 
